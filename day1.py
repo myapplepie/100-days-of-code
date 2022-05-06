4dfd787e0b8f3b3eff1a1fbed02d9103de0a1d6b
myapplepie <97968970+myapplepie@users.noreply.github.com>
2022-05-06 22:35:43 +0300

    Add files via upload
    Day 1 "Band name generator"

---
+++ day1.py
@@ -0,0 +1,5 @@
+print("Welcome to the band name generator")
+city = input("What is the name of your hometown? \n")
+pet = input("what is the name of your pet? \n")
+color = input("what is your favorite color? \n")
+print("Your band name is: " + color + " " + pet + " " + city)
